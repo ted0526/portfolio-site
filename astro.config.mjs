737e7a98d821df56a4f4549ae8ebf778065d925f
ted0526 <tedpaek@yahoo.com>
2025-05-19 02:43:39 -0400

    progress

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,20 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
+import react from '@astrojs/react';
+
 
 export default defineConfig({
   site: 'https://example.com',
   integrations: [
+	react(),
     mdx(),
     sitemap(),
     icon({
       // Tell astro-icon which sets and which glyphs you need:
       collections: {
         mdi: {
-          included: ['linkedin', 'youtube', 'github'],
+          included: ['*'],
         },
       },
     }),
---
+++ src/components/GhCalendar.jsx
@@ -0,0 +1,7 @@
+// src/components/GhCalendar.jsx
+import React from 'react';
+import GitHubCalendar from 'react-github-calendar';
+
+export default function GhCalendar(props) {
+  return <GitHubCalendar {...props} />;
+}
